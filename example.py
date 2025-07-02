ce428fdb5f357ed5bc851de9634e31678be27f42
ayanda <past952@gmail.com>
2025-07-02 22:27:40 +0200

    Cleanup: remove unused files and update .gitignore to include additional patterns

--- example.py
+++
@@ -1,6 +0,0 @@
-def print_triangle(rows):
-    for i in range(1, rows + 1):
-        print(" " * (rows - i) + "/" + "  " * (i - 1) + "\\")
-    print("/" + "__" * (rows - 1) + "\\")
-
-print_triangle(5)
